2dfd72741435d582427ce702b8871cabada710be
Davi-Marques <93229005+davi-marques@users.noreply.github.com>
2022-01-06 10:54:35 -0400

    comentário 3

--- script.js
+++ script.js
@@ -46,6 +46,6 @@ function verificar() {
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        res.appendChild(img)
+        res.appendChild(img) // Adiciona o resultado a mais
     }
 }
